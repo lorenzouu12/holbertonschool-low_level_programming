51243c1dbde89fcd7928b9338513fbc96806d5c9
lorenzouu12 <lorenzochristian80@gmail.com>
2025-02-19 13:56:52 -0400

    task_3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,8 @@
 /**
 * main - Entry point
 *
-* Description: Print the alphabet in lowercase, and uppercase
+* Description: Print the alphabet in lowercase, and uppercase,
+* then in uppercase, followed by a new line.
 *
 * Return: Always 0
 */
