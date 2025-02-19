ba096e8dd75f4935ceff4ccaa74f7f1cca3f6475
lorenzouu12 <lorenzochristian80@gmail.com>
2025-02-19 09:54:04 -0400

    print

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Descrption: Print the alphabet in lower case
+ *
+ * Return : Always 0
+ */
+
+int main(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+puchar (letter);
+letter++;
+}
+putchar('\n');
+
+return (0);
+}
